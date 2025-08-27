b6476e147f6da8ca4c14d81a0dce8f390b1a3a15
alikpashaevi <alikpashaev06@gmail.com>
2025-08-27 15:00:50 +0400

    failed oauth2 impl

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,8 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("org.springframework.security:spring-security-oauth2-client")
 }
 
 dependencyManagement {
--- src/main/java/alik/receiptmaker/user/persistence/AppUserRepo.java
+++ src/main/java/alik/receiptmaker/user/persistence/AppUserRepo.java
@@ -11,4 +11,6 @@ public interface AppUserRepo extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findByUsername(String username);
 
     boolean existsByUsername(String username);
+
+    Optional<AppUser> findByEmail(String email);
 }
