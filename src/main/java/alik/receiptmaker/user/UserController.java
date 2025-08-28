d35f2aee8c40e89381c85feb972bfa00f2dd0e17
alikpashaevi <alikpashaev06@gmail.com>
2025-08-28 17:19:50 +0400

    change username and password methods

--- src/main/java/alik/receiptmaker/user/UserController.java
+++ src/main/java/alik/receiptmaker/user/UserController.java
@@ -3,6 +3,7 @@ package alik.receiptmaker.user;
 import alik.receiptmaker.user.model.UserRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,15 +24,29 @@ public class UserController {
 //    }
 
     @PreAuthorize(USER_OR_ADMIN)
+    @PutMapping
+    public void changePassword(@RequestParam String oldPassword, @RequestParam String newPassword) {
+        userService.changePassword(oldPassword, newPassword);
+    }
+
+    @PreAuthorize(USER_OR_ADMIN)
+    @PutMapping
+    public void changeUsername(@RequestParam String newUsername) {
+        userService.changeUsername(newUsername);
+    }
+
+    @PreAuthorize(USER_OR_ADMIN)
     @PostMapping("/favorites/{id}")
-    public void addToFavorites(@PathVariable Long id) {
+    public ResponseEntity<String> addToFavorites(@PathVariable Long id) {
         userService.addToFavorites(id);
+        return ResponseEntity.ok("Recipe added to favorites");
     }
 
     @PreAuthorize(USER_OR_ADMIN)
     @DeleteMapping("/favorites/{id}")
-    public void removeFromFavorites(@PathVariable Long id) {
+    public ResponseEntity<String> removeFromFavorites(@PathVariable Long id) {
         userService.removeFromFavorites(id);
+        return ResponseEntity.ok("Recipe removed from favorites");
     }
 
 }
